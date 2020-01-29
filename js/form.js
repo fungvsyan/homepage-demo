aec02efb9bfc872adac690d2365a42d3c6839925
Alex, Suetyan Mok <43975190+fungvsyan@users.noreply.github.com>
2020-01-29 14:57:17 +0800

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,8 @@
+function print_result() {
+	var name = document.f1.name.value;
+	alert("Name: " + name);
+	var country = document.f1.country.value;
+	alert("Country: " + country);
+	var comment = document.f1.comment.value;
+	alert("Comment: " + comment);
+}
